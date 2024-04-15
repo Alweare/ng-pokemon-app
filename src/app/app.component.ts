7df35d8f2a6cf7af66e2266659cf240f4100ec8f
Alweare <yanis.dumont@gmail.com>
2024-04-15 07:45:17 +0200

    mise en place de la liste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
+	selector: "app-root",
+	template: `
+		<h1>Liste de Pokémons !</h1>
 
-    <router-outlet />
-  `,
-  styles: []
+		<router-outlet />
+	`,
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+	
+pokemonList: Pokemon[] = POKEMONS;
+
+	ngOnInit() { // méthode qui se lance a l'initialisation. 
+		console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[1]);
+	}
+
+	selectPokemon(pokemon: Pokemon)
+     {
+		console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+	}
+
 }
+//modifier le template pour afficher liste de pokemon 
+//charger la liste des 12 Pokemon
+//afficher le nom d'un pokemon dans le console log 
