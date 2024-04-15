f089b256d1b086bc9faf7846fad1b129c0476466
Alweare <yanis.dumont@gmail.com>
2024-04-15 09:18:33 +0200

    Boucle dans un tableau de donnée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,27 +4,30 @@ import { Pokemon } from "./pokemon";
 
 @Component({
 	selector: "app-root",
-	template: `
-		<h1>Liste de Pokémons !</h1>
-
-		<router-outlet />
-	`,
+	templateUrl: "app.component.html",
 })
 export class AppComponent implements OnInit {
-	
-pokemonList: Pokemon[] = POKEMONS;
-
-	ngOnInit() { // méthode qui se lance a l'initialisation. 
+	pokemonList: Pokemon[] = POKEMONS;
+	pokemonSelected: Pokemon|undefined;
+	ngOnInit() {
+		// méthode qui se lance a l'initialisation.
 		console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[1]);
 	}
 
-	selectPokemon(pokemon: Pokemon)
-     {
-		console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+	selectPokemon(pokemonId: String) {
+		const id = +pokemonId;
+		const pokemon: Pokemon | undefined = this.pokemonList.find(
+			(pokemon) => pokemon.id == +pokemonId
+		);
+		if (pokemon) {
+			console.log(`Vous avez demandé ${pokemon.name}`);
+			this.pokemonSelected = pokemon;
+		} else {
+			console.log(`Vous avez demandé un pokemon qui n'existe pas`);
+			this.pokemonSelected = pokemon;
+		}
 	}
-
 }
-//modifier le template pour afficher liste de pokemon 
+//modifier le template pour afficher liste de pokemon
 //charger la liste des 12 Pokemon
-//afficher le nom d'un pokemon dans le console log 
+//afficher le nom d'un pokemon dans le console log
