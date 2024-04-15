e98a6b97965cfd2ea8149527cf9be144b59c8f6e
Alweare <yanis.dumont@gmail.com>
2024-04-15 15:19:58 +0200

    création de deux compnents pour commencer a mettre en place les routes

---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html',
+  styles: ``
+})
+export class DetailPokemonComponent {
+
+}
---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+  styles: ``
+})
+export class ListPokemonComponent {
+
+}
