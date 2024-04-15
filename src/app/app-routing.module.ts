1f405207322ef116548515fb0048eb4402b04be6
Alweare <yanis.dumont@gmail.com>
2024-04-15 16:14:48 +0200

    page 404 not found implémentée

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
 	{ path: "pokemons", component: ListPokemonComponent }, //les routes sont lu du haut vers le bas faire les plus spécifique en haut et général en bas
 	{ path: "pokemon/:id", component: DetailPokemonComponent },
 	{ path: "", redirectTo: "pokemons", pathMatch: "full" },
+	{ path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
