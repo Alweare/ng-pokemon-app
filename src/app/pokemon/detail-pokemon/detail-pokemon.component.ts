30a5b2b1e00c17e6fef67fec19a882702bbf5788
Alweare <yanis.dumont@gmail.com>
2024-04-17 15:58:27 +0200

    refacto et injection service au niveau module

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { POKEMONS } from "../mock-pokemon-list";
 import { Pokemon } from "../pokemon";
 import { Router } from "@angular/router";
+import { PokemonService } from "../pokemon.service";
 @Component({
 	selector: "app-detail-pokemon",
 	templateUrl: "./detail-pokemon.component.html",
@@ -12,16 +12,17 @@ export class DetailPokemonComponent implements OnInit {
 	pokemonList: Pokemon[];
 	pokemon: Pokemon | undefined;
 
-	constructor(private route: ActivatedRoute, private router: Router) {}
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private pokemonService: PokemonService
+	) {}
 
 	ngOnInit() {
-		this.pokemonList = POKEMONS;
 		const pokemonId: string | null = this.route.snapshot.paramMap.get("id");
 
 		if (pokemonId) {
-			this.pokemon = this.pokemonList.find(
-				(pokemon) => pokemon.id == +pokemonId
-			);
+			this.pokemon = this.pokemonService.getPokemonbyId(+pokemonId);
 		} else {
 			this.pokemon = undefined;
 		}
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -5,6 +5,7 @@ import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { RouterModule, Routes } from "@angular/router";
+import { PokemonService } from "./pokemon.service";
 const pokemonRoutes: Routes = [
 	{ path: "pokemons", component: ListPokemonComponent }, //les routes sont lu du haut vers le bas faire les plus spécifique en haut et général en bas
 	{ path: "pokemon/:id", component: DetailPokemonComponent },
@@ -17,5 +18,6 @@ const pokemonRoutes: Routes = [
 		DetailPokemonComponent,
 	],
 	imports: [CommonModule, RouterModule.forChild(pokemonRoutes)],
+  providers: [PokemonService]
 })
 export class PokemonModule {}
