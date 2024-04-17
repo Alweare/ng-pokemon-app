ea022de3a537923b8eb2dc7321f162398ef397cc
Alweare <yanis.dumont@gmail.com>
2024-04-17 14:50:14 +0200

    Restructuration de l'app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import {
+	BrowserModule,
+	provideClientHydration,
+} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BorderCardDirective } from "./pokemon/border-card.directive";
+import { PokemonTypeColorPipe } from "./pokemon/pokemon-type-color.pipe";
+import { ListPokemonComponent } from "./pokemon/list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./pokemon/detail-pokemon/detail-pokemon.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    PageNotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		BorderCardDirective,
+		PokemonTypeColorPipe,
+		ListPokemonComponent,
+		DetailPokemonComponent,
+		PageNotFoundComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [provideClientHydration()],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
